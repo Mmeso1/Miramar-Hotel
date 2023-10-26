cf5236ecf5368bb2847ac158e32ea509df0fef0d
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2023-10-26 14:56:40 +0100

    added routes for error pages and custom styles block for custom css linking

--- app/views/user.py
+++ app/views/user.py
@@ -7,3 +7,11 @@ user = Blueprint("user", __name__)
 @user.route("/")
 def landing_page():
     return render_template("user/landing.html")
+
+@user.route("/error_404") 
+def error_404():
+    return render_template('error_404.html')
+
+@user.route("/error_500") 
+def error_500():
+    return render_template('error_500.html')
