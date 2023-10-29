f0ad9de65ceaf3e8de831841cb830e906ba0da33
Mmeso1 <anisiubammesoma8@gmail.com>
2023-10-29 20:49:49 +0100

    Modularizing and routing th admin page

--- app/views/admin.py
+++ app/views/admin.py
@@ -2,4 +2,40 @@ from flask import Blueprint, render_template
 
 admin = Blueprint("admin", __name__) 
 
-# In the init.py I prefixed the admin's route with /admin, so when you want to render a page's view, if the current route you named is /dashboard, you'll now type /admin/dashboard in the browser
+@admin.route("/")
+@admin.route("/home")
+def home_page():
+    return render_template("admin/index.html", page="Dashboard")
+
+@admin.route("/contact")
+def contact_page():
+    return render_template("admin/contacts.html", page="Contact")
+
+@admin.route("/profile")
+def profile_page():
+    return render_template("admin/app-profile.html", page="Profile")
+
+@admin.route("/message")
+def message_page():
+    return render_template("admin/message.html", page="Message")
+
+@admin.route("/calendar")
+def calendar_page():
+    return render_template("admin/app-calender.html", page="Calendar")
+
+@admin.route("/gallery")
+def price_page():
+    return render_template("admin/uc-lightgallery.html", page="Gallery") #ecom-product-grid.html
+
+@admin.route("/room")
+def room_page():
+    return render_template("admin/ecom-product-list.html", page="Rooms")
+
+@admin.route("/order")
+def order_page():
+    return render_template("admin/ecom-product-order.html", page="Booking Requests")
+
+@admin.route("/customer")
+def customer_page():
+    return render_template("admin/ecom-customers.html", page="Customers")
+
