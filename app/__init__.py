2270ed725a8111bcaccaea185bad467f2763eef1
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2023-11-01 10:29:50 +0100

    saving changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from .config.variables import SECRET_KEY 
 from flask_mail import Mail, Message 
 import os
