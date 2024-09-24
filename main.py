93a00309a18fd43dbba68e65eb03bd54f5780dc8
Web3Phoenix <117487194+chidubesteve@users.noreply.github.com>
2024-09-24 02:46:28 +0100

    Update main.py - deploy to production
    changed debug state to false

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from app import create_app
 # __name__ CHANGES TO __main__ WHENEVER THE FILE IS RUNNING
 if __name__ == "__main__":
   app = create_app()
-  app.run(debug=True, port = 3000) # FOR DEVELOPMENT PURPOSE      
+  app.run(debug=False) # FOR DEVELOPMENT PURPOSE      
